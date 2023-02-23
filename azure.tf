4db2603e31cb1558dfc132115cd5caca95fed410
Sarmad Saleem <sarmad.saleem@live.com>
2023-02-23 23:52:16 +0100

    Setup app gateway

--- azure.tf
+++ azure.tf
@@ -1,25 +1,151 @@
+###########
+# Provider
+###########
 provider "azurerm" {
   subscription_id = var.azure_subscription_id
   features {}
   skip_provider_registration = true
 }
 
+#########
+# Locals
+#########
+locals {
+  prefix                         = "cf-az-testdrive"
+  backend_address_pool_name      = "${azurerm_virtual_network.vnet.name}-beap"
+  frontend_port_name             = "${azurerm_virtual_network.vnet.name}-feport"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.vnet.name}-feip"
+  http_setting_name              = "${azurerm_virtual_network.vnet.name}-be-htst"
+  listener_name                  = "${azurerm_virtual_network.vnet.name}-httplstn"
+  request_routing_rule_name      = "${azurerm_virtual_network.vnet.name}-rqrt"
+  redirect_configuration_name    = "${azurerm_virtual_network.vnet.name}-rdrcfg"
+}
+
+#################
+# Resource group
+#################
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-cf-az-test-drive"
+  name     = "${local.prefix}-rg"
   location = "West Europe"
 }
 
+##########
+# Network
+##########
+resource "azurerm_virtual_network" "vnet" {
+  name                = "network"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.254.0.0/16"]
+}
+
+resource "azurerm_subnet" "frontend" {
+  name                 = "frontend"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.254.0.0/24"]
+}
+
+resource "azurerm_subnet" "backend" {
+  name                 = "backend"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.254.2.0/24"]
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name = "Microsoft.ContainerInstance/containerGroups"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/action"
+      ]
+    }
+  }
+}
+
+##############
+# App gateway
+##############
+resource "azurerm_public_ip" "pip" {
+  name                = "pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  sku_tier            = "Regional"
+}
+
+resource "azurerm_application_gateway" "ng" {
+  name                = "appgateway"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 1
+  }
+
+  gateway_ip_configuration {
+    name      = "gateway-ip-configuration"
+    subnet_id = azurerm_subnet.frontend.id
+  }
+
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+
+  backend_address_pool {
+    name         = local.backend_address_pool_name
+    ip_addresses = [azurerm_container_group.cg.ip_address]
+  }
+
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    path                  = "/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+    priority                   = 1
+  }
+}
+
+######################
+# Container Instances
+######################
 resource "azurerm_container_group" "cg" {
-  name                = "cg-cf-az-test-drive"
+  name                = "${local.prefix}-cg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  ip_address_type     = "Public"
-  dns_name_label      = "cg-cf-az-test-drive"
+  ip_address_type     = "Private"
+  subnet_ids          = [azurerm_subnet.backend.id]
   os_type             = "Linux"
 
   container {
     name   = "hello-world"
-    image  = "nginx:latest"
+    image  = "nginx:1.23.3"
     cpu    = "0.5"
     memory = "1.5"
 
--- cloudflare.tf
+++ cloudflare.tf
@@ -1,11 +1,19 @@
+###########
+# Provider
+###########
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
+##########
+# Records
+##########
+# Note: In this case we are using existing zone, this can also be created
+# within Terraform as well using cloudflare_zone if needed
 resource "cloudflare_record" "app" {
   zone_id = var.zone_id
   name    = "app"
-  value   = azurerm_container_group.cg.ip_address
+  value   = azurerm_public_ip.pip.ip_address
   type    = "A"
   proxied = true
 }
